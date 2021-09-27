929e5779e88161f4c83eb26ad5997a0bea8a31e4
Kamil Wcisło <mek.xgt@gmail.com>
2021-09-27 13:19:47 +0200

    add accumulated statistics

--- main.go
+++ main.go
@@ -24,12 +24,14 @@ var config struct {
 	interval time.Duration
 	binary   *bool
 	mode     *int
+	leaveStale *bool
 	modeName string
 }
 
 type twoP struct {
 	first, second      process.Process
 	diff, diffR, diffW int
+	accu, accuR, accuW int
 	exited             bool
 }
 type mapP map[string]twoP
@@ -69,21 +71,28 @@ func sortP(lastP *mapP) *list.List {
 		seconds := int32(config.interval.Seconds())
 		if val.first.Timestamp+seconds < nowTimestamp {
 			// Schedule remove obsolete pids from lastP
-			remove.PushBack(val.first.Id)
+			if !(*config.leaveStale) {
+				remove.PushBack(val.first.Id)
+			}
 			// Display this process one more time, but in a fancy way
 			val.exited = true
 		}
 
-		if val.diff > 0 {
+		if val.accu > 0 {
 			// Insertion sort
 			if sorted.Len() > 0 {
+				inserted := false
 				for e := sorted.Front(); e != nil; e = e.Next() {
-					diff := e.Value.(twoP).diff
-					if diff < val.diff {
-						//fmt.Printf("Inserting %d before %d\n", val.diff, diff)
+					accu := e.Value.(twoP).accu
+					if val.accu >= accu {
+						//fmt.Printf("Inserting %d before %d\n", val.first.Pid, e.Value.(twoP).first.Pid)
 						sorted.InsertBefore(val, e)
+						inserted = true
 						break
 					}
+				}
+				if !inserted {
+					sorted.PushBack(val)
 				}
 			} else {
 				sorted.PushFront(val)
@@ -111,7 +120,7 @@ func printP(sortedP *list.List) {
 	fmt.Printf("\033[H\033[2J")
 	fmt.Printf("Mode: %s, Interval: %s\n", config.modeName, config.interval)
 	fmt.Printf("(Hit Ctr-C to quit, re-run with -h for flags)\n")
-	fmt.Printf("%5s/%5s %5s %s\n", "WRITE", "READS", "PID", "COMMAND")
+	fmt.Printf("%5s %5s %5s %5s %5s %s\n", "WRITE", "READS", "W/s", "R/s", "PID", "COMMAND")
 
 	// Print the results
 	row := 3
@@ -124,21 +133,28 @@ func printP(sortedP *list.List) {
 		first := val.first
 
 		var outstr string
+		var cmdstr string
+		var W, R, Ws, Rs string
+
+		if *config.binary {
+			W, R = utils.HumanBinary(val.accuW), utils.HumanBinary(val.accuR)
+			Ws, Rs = utils.HumanBinary(val.diffW), utils.HumanBinary(val.diffR)
+		} else {
+			W, R = utils.Human(val.accuW), utils.Human(val.accuR)
+			Ws, Rs = utils.Human(val.diffW), utils.Human(val.diffR)
+		}
 
 		if val.exited {
-			outstr = fmt.Sprintf("XXXXXXXXXXX %5d %s", first.Pid, first.Cmdline)
-
+			cmdstr = "[exited]" + first.Cmdline
 		} else {
-			var humanW, humanR string
-
-			if *config.binary {
-				humanW, humanR = utils.HumanBinary(val.diffW), utils.HumanBinary(val.diffR)
-			} else {
-				humanW, humanR = utils.Human(val.diffW), utils.Human(val.diffR)
-			}
-
-			outstr = fmt.Sprintf("%5s %5s %5d %s", humanW, humanR, first.Pid, first.Cmdline)
+			cmdstr = first.Cmdline
 		}
+
+		outstr = fmt.Sprintf("%5s %5s %5s %5s %5d %s",
+			W, R,
+			Ws, Rs,
+			first.Pid,
+			cmdstr)
 
 		l := len(outstr)
 		if l > tWidth {
@@ -162,6 +178,13 @@ func modeNames() (string, string, string, error) {
 	return "", "", "", errors.New(errstr)
 }
 
+func max(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
+}
+
 func receiveP(pRxChan <-chan process.Process) {
 	lastP := make(mapP)
 	flag := false
@@ -171,19 +194,23 @@ func receiveP(pRxChan <-chan process.Process) {
 		log.Fatal(err)
 	}
 	config.modeName = modeName
-	seconds := config.interval.Seconds()
+	seconds := float64(config.interval.Milliseconds()) / 1000.0
 
 	makeDiff := func(first, second process.Process) twoP {
 		firstValR, firstValW := first.Count[readKey], first.Count[writeKey]
 		secondValR, secondValW := second.Count[readKey], second.Count[writeKey]
 		diffR, diffW := utils.Abs(firstValR-secondValR), utils.Abs(firstValW-secondValW)
 
+		accuR := max(first.Count[readKey], second.Count[readKey])
+		accuW := max(first.Count[writeKey], second.Count[writeKey])
+		accu := accuR + accuW
+
 		// Calculate averages, so we have always per second valus
 		diff := int(float64(diffR+diffW) / seconds)
 		diffR = int(float64(diffR) / seconds)
 		diffW = int(float64(diffW) / seconds)
 
-		return twoP{first, second, diff, diffR, diffW, false}
+		return twoP{first, second, diff, diffR, diffW, accu, accuR, accuW, false}
 	}
 
 	for p := range pRxChan {
@@ -200,7 +227,11 @@ func receiveP(pRxChan <-chan process.Process) {
 					lastP[p.Id] = makeDiff(p, val.second)
 				}
 			} else {
-				lastP[p.Id] = twoP{first: p}
+				lastP[p.Id] = twoP{
+					first: p,
+					accu: p.Count[readKey] + p.Count[writeKey],
+					accuW: p.Count[writeKey],
+					accuR: p.Count[readKey]}
 			}
 		}
 	}
@@ -208,10 +239,11 @@ func receiveP(pRxChan <-chan process.Process) {
 
 func parseFlags() {
 	helpF := flag.Bool("v", false, "Print the version")
-	interF := flag.Int("i", 1, "Update interval in seconds")
+	interF := flag.Float64("i", 1.0, "Update interval in seconds")
 
 	config.binary = flag.Bool("b", false, "Use binary instead of decimal (e.g. kiB an not kB)")
 	config.mode = flag.Int("m", 0, "The stats mode: 0:bytes 1:syscalls 2:chars")
+	config.leaveStale = flag.Bool("l", false, "Leave stale processes on the list")
 
 	flag.Parse()
 
@@ -222,7 +254,7 @@ func parseFlags() {
 		os.Exit(0)
 	}
 
-	config.interval = time.Duration(*interF) * time.Second
+	config.interval = time.Duration(*interF * 1000) * time.Millisecond
 }
 
 func main() {
